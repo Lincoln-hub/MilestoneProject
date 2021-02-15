cbd9b5b969ae1d86976a6c223915f209745eb8ed
Lincoln-hub <72630978+Lincoln-hub@users.noreply.github.com>
2021-02-15 11:59:33 -0700

    fixed header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,19 @@
 
 <!-- /.Heading -->
-<h2>Welcome to Activity 2</h2>
+
+<style>
+    .footer
+    {
+    
+        text-align: center;
+        position: relative;
+        bottom: 0;
+        width: 100%;
+        clear: both;
+        
+    }
+</style>
+
+<div class="footer" align="center">
+   <h2>CLC-Milestone</h2>
+</div>
